dc6c284c39fc3f354db6479ad5dc8193db692c6f
Emilien Boulben <emilien.boulben@gmail.com>
2015-01-14 17:13:59 +0100

    WAPP : demonstration logger

--- wapp/src/main/java/com/isep/javaeeproject/web/home/HomeController.java
+++ wapp/src/main/java/com/isep/javaeeproject/web/home/HomeController.java
@@ -2,6 +2,8 @@ package com.isep.javaeeproject.web.home;
 
 import com.isep.javaeeproject.web.mapping.Views;
 import com.isep.javaeeproject.web.mapping.WebMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,8 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(WebMapping.HOME)
 public class HomeController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping
     public ModelAndView home() {
+        logger.info("home Controller logger demonstration");
         return new ModelAndView(Views.HOME.getPath());
     }
 
