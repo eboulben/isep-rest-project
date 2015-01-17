d0e24ea7ec1a748d3fda6c7937fda47b41382523
Emilien Boulben <emilien.boulben@gmail.com>
2015-01-18 00:03:50 +0100

    REST : better sql script and data

--- rest/src/main/resources/sql/create-table.sql
+++ rest/src/main/resources/sql/create-table.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `rest_tweets`;
 DROP TABLE IF EXISTS `rest_tweets`.`tweets`;
 
 CREATE TABLE `rest_tweets`.`tweets` (
-  `id`        BIGINT      NOT NULL AUTO_INCREMENT,
+  `id`        BIGINT      NOT NULL AUTO_INCREMENT UNIQUE ,
   `id_tweets` BIGINT      NOT NULL,
   `id_author` BIGINT      NOT NULL,
   `author`    VARCHAR(50) NOT NULL,
--- rest/src/main/resources/sql/populate-table.sql
+++ rest/src/main/resources/sql/populate-table.sql
@@ -3,13 +3,13 @@ LOCK TABLES `rest_tweets`.`tweets` WRITE;
 INSERT INTO `rest_tweets`.`tweets`
 (id_tweets, id_author, author, message, date)
 VALUES
-  (9831683518, 318465498, '@NagaSadow',
+  (9831683518, 318465498, 'NagaSadow',
    'The Light Side chose Linux cause it has the power to liberate everyone. The dark Side chose it cause this power is unlimited',
    '2014-05-04 05:04:42'),
-  (843516834, 351986518, '@ElrosMinyatur',
+  (843516834, 351986518, 'ElrosMinyatur',
    'On Elenna we founded the most beautiful realm that could exist, too bad we destroyed it all by madness!',
    '2014-12-01 09:30:42'),
-  (9843516846, 9843516835, '@d_adams',
+  (9843516846, 9843516835, 'd_adams',
    'What will the second most powerful computer answer if a mouse comes looking for some stupid answer ?',
    '2014-10-10 10:42:42');
 
